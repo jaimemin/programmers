99be2c5f4ff6f78fd2185dd656a78ef4dfc0e4a1
Jaime Min <jaimemin@gmail.com>
2022-01-13 23:35:54 +0900

    2022.01.13

---
+++ 나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+	vector<int> answer;
+
+	for (int num : arr)
+	{
+		if (num % divisor == 0)
+		{
+			answer.push_back(num);
+		}
+	}
+
+	if (answer.empty())
+	{
+		answer.push_back(-1);
+	}
+
+	sort(answer.begin(), answer.end());
+
+	return answer;
+}
