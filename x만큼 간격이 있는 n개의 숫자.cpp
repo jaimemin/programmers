8e4b9c734556a2e9a7f0b69acc26a60f56f9f022
Jaime Min <jaimemin@gmail.com>
2022-03-01 23:22:05 +0900

    2022.03.01

---
+++ x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+	vector<long long> answer;
+
+	for (int i = 0; i < n; i++)
+	{
+		answer.push_back((i + 1) * x * 1LL);
+	}
+
+	return answer;
+}
