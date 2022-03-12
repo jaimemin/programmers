bb7403b6aafdabc790ca0d4a67f098563acd32c2
Jaime Min <jaimemin@gmail.com>
2022-03-12 20:49:25 +0900

    2022.03.12

---
+++ 행렬의 곱셈.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+	vector<vector<int>> answer;
+
+	for (int i = 0; i < arr1.size(); i++)
+	{
+		vector<int> temp;
+		int sum = 0;
+
+		for (int k = 0; k < arr2[0].size(); k++)
+		{
+			for (int j = 0; j < arr1[i].size(); j++)
+			{
+				sum += arr1[i][j] * arr2[j][k];
+			}
+
+			temp.push_back(sum);
+			sum = 0;
+		}
+
+		answer.push_back(temp);
+	}
+
+	return answer;
+}
