df9ac5ab082f2e68bac8dd3e8d714972678dd136
jaimemin <jaimemin@gmail.com>
2022-08-08 22:34:35 +0900

    2022.08.08

---
+++ 외벽 점검.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <iostream>
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+	sort(dist.begin(), dist.end());
+
+	int weakSize = weak.size();
+	weak.resize(weakSize * 2);
+
+	for (int i = 0; i < weakSize; i++)
+	{
+		weak[i + weakSize] = weak[i] + n;
+	}
+
+	int answer = INT_MAX;
+
+	do
+	{
+		for (int i = 0; i < weakSize; i++)
+		{
+			int left = weak[i];
+			int right = weak[i + weakSize - 1];
+			int idx = i;
+
+			for (int j = 0; j < dist.size(); j++)
+			{
+				if (right - left <= dist[j])
+				{
+					answer = min(answer, j + 1);
+
+					break;
+				}
+
+				for (int k = idx + 1; weakSize * 2; k++)
+				{
+					if (left + dist[j] < weak[k])
+					{
+						left = weak[k];
+						idx = k;
+
+						break;
+					}
+				}
+			}
+		}
+	} while (next_permutation(dist.begin(), dist.end()));
+
+	return answer == INT_MAX ? -1 : answer;
+}
