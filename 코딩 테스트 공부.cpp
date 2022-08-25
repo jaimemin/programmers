84f593a24b19b26e4e935c212f96f200c44164a9
jaimemin <jaimemin@gmail.com>
2022-08-26 00:11:34 +0900

    Update 코딩 테스트 공부.cpp

--- 코딩 테스트 공부.cpp
+++ 코딩 테스트 공부.cpp
@@ -14,55 +14,44 @@ int maxCopReq = 0;
 
 int cache[MAX][MAX];
 
-void init(void)
-{
-    for (int i = 0; i < MAX; i++)
-    {
-        for (int j = 0; j < MAX; j++)
-        {
-            cache[i][j] = INF; 
-        }
-    }
-}
-
-int func(int alp, int cop, vector<vector<int>> &problems)
+int func(int alp, int cop, vector<vector<int>>& problems)
 {
     if (maxAlpReq <= alp && maxCopReq <= cop)
     {
         return 0;
     }
-    
-    int &result = cache[alp][cop];
-    
+
+    int& result = cache[alp][cop];
+
     if (result != -1)
     {
         return result;
     }
-    
+
     result = INF;
     result = min(result, func(min(maxAlpReq, alp + 1), cop, problems) + 1);
     result = min(result, func(alp, min(maxCopReq, cop + 1), problems) + 1);
-    
+
     for (vector<int> problem : problems)
     {
         if (alp < problem[0] || cop < problem[1])
         {
             continue;
         }
-        
+
         result = min(result, func(min(maxAlpReq, alp + problem[2]), min(maxCopReq, cop + problem[3]), problems) + problem[4]);
     }
-    
+
     return result;
 }
 
 int solution(int alp, int cop, vector<vector<int>> problems) {
     memset(cache, -1, sizeof(cache));
-    
+
     for (vector<int> problem : problems) {
         maxAlpReq = max(maxAlpReq, problem[0]);
         maxCopReq = max(maxCopReq, problem[1]);
     }
-    
+
     return func(alp, cop, problems);
 }
