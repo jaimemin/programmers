0bc2cee693b5741530c8ece74524c150b250e988
jaimemin <jaimemin@gmail.com>
2023-01-01 23:20:34 +0900

    2023.01.01

---
+++ n의 배수 고르기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> numlist) {
+    vector<int> answer;
+
+    for (int num : numlist)
+    {
+        if (num % n == 0)
+        {
+            answer.push_back(num);
+        }
+    }
+
+    return answer;
+}
