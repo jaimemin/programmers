b40c3efa2553fd1877026c25754c9e571a01e9cc
jaimemin <jaimemin@gmail.com>
2023-03-08 23:19:11 +0900

    2023.03.08

---
+++ 즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+SELECT
+    FOOD_TYPE,
+    REST_ID,
+    REST_NAME,
+    FAVORITES
+FROM
+(
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS RN
+    FROM
+        REST_INFO
+    
+) AS REST_INFO
+WHERE
+    RN = 1
+ORDER BY
+    FOOD_TYPE DESC
