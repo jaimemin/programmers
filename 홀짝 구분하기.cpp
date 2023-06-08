e477b7546a3c0007c19085f16d10dad387fb090b
jaimemin <jaimemin@gmail.com>
2023-06-08 23:58:01 +0900

    2023.06.08

---
+++ 홀짝 구분하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    
+    if (n % 2)
+    {
+        cout << n << " is odd\n";
+    }
+    else
+    {
+        cout << n << " is even\n";
+    }
+    
+    return 0;
+}
