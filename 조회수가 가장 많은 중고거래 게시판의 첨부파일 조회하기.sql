b03759ec0a519633e77f300cad56b2ad6e174fe9
jaimemin <jaimemin@gmail.com>
2023-06-16 23:48:45 +0900

    2023.06.16

---
+++ 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT
+    CONCAT("/home/grep/src/", UGB.BOARD_ID, "/", FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM
+    USED_GOODS_BOARD AS UGB
+INNER JOIN
+    USED_GOODS_FILE AS UGF
+ON
+    UGB.BOARD_ID = UGF.BOARD_ID
+WHERE
+    VIEWS = (
+        SELECT 
+            MAX(VIEWS) 
+        FROM 
+            USED_GOODS_BOARD
+    )
+ORDER BY
+    FILE_ID DESC
