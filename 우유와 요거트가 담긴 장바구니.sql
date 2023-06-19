5f95dc1eaacb10bc446cec9cf9615eb8f6fe340a
jaimemin <jaimemin@gmail.com>
2023-06-20 00:00:45 +0900

    2023.06.20

---
+++ 우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,25 @@
+-- 코드를 입력하세요
+SELECT
+    DISTINCT(CART_ID)
+FROM
+    CART_PRODUCTS
+WHERE
+    CART_ID IN (
+        SELECT
+            CART_ID
+        FROM
+            CART_PRODUCTS
+        WHERE
+            NAME = "Milk"
+    )
+AND
+    CART_ID IN (
+        SELECT
+            CART_ID
+        FROM
+            CART_PRODUCTS
+        WHERE
+            NAME = "Yogurt"
+    )
+ORDER BY
+    CART_ID
