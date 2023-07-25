18901a8f9bfa7aa8e599002d58c0eee778d7d8ba
jaimemin <jaimemin@gmail.com>
2023-07-25 23:49:11 +0900

    2023.07.25

---
+++ 암호 해독.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    
+    for (int i = code - 1; i < cipher.length(); i += code)
+    {
+        answer += cipher[i];
+    }
+    
+    return answer;
+}
