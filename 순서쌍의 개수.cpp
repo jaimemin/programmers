104d27d3ffd493a31b5ba45c925df242d2766934
jaimemin <jaimemin@gmail.com>
2023-10-10 23:47:53 +0900

    2023.10.10

---
+++ 순서쌍의 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        answer += (n % i == 0);
+    }
+    
+    return answer;
+}
