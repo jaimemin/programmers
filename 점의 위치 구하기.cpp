fc5d54af31d1c9662f6db485fd735a9b9247133a
jaimemin <jaimemin@gmail.com>
2023-10-26 23:58:52 +0900

    Create 점의 위치 구하기.cpp

---
+++ 점의 위치 구하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    if (dot[0] > 0 && dot[1] > 0)
+    {
+        return 1;
+    }
+    else if (dot[0] < 0 && dot[1] > 0)
+    {
+        return 2;
+    }
+    else if (dot[0] < 0 && dot[1] < 0)
+    {
+        return 3;
+    }
+    
+    return 4;
+}
