c99f35926d8d6d3c9ea327b22098be11c572fb63
jaimemin <jaimemin@gmail.com>
2024-07-12 23:40:36 +0900

    2024.07.12

---
+++ 부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,13 @@
+-- 코드를 작성해주세요
+SELECT 
+    d.DEPT_ID, 
+    d.DEPT_NAME_EN, 
+    ROUND(AVG(e.SAL), 0) AS AVG_SAL
+FROM 
+    HR_DEPARTMENT d
+INNER JOIN 
+    HR_EMPLOYEES e ON d.DEPT_ID = e.DEPT_ID
+GROUP BY 
+    d.DEPT_ID, d.DEPT_NAME_EN
+ORDER BY 
+    AVG_SAL DESC;
