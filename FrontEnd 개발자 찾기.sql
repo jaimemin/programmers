13dfbe98e6577ac30ad4ede66582ec91b405b503
jaimemin <jaimemin@gmail.com>
2024-07-15 16:38:04 +0900

    2024.07.15

---
+++ FrontEnd 개발자 찾기.sql
@@ -0,0 +1,21 @@
+-- 코드를 작성해주세요
+SELECT
+    D.ID,
+    D.EMAIL,
+    D.FIRST_NAME,
+    D.LAST_NAME
+FROM
+    DEVELOPERS D
+JOIN
+    SKILLCODES S
+ON
+    (D.SKILL_CODE & S.CODE) != 0
+WHERE
+    S.CATEGORY = 'Front End'
+GROUP BY
+    D.ID, 
+    D.EMAIL, 
+    D.FIRST_NAME, 
+    D.LAST_NAME
+ORDER BY
+    D.ID ASC;
