9a5e5671219f98dd6d0cd54cbde431747b802e65
jaimemin <jaimemin@gmail.com>
2024-10-02 23:23:10 +0900

    2024.10.02

---
+++ [PCCE 기출문제] 1번 문자 출력.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "Let's go!";
+
+    cout << "3" << endl << 2 << endl << "1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
